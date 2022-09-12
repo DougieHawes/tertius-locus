1b28cbf4edded3bf8baea56c54d8aac7d758ac11
dougiehawes <dougiehawes@hotmail.com>
2022-09-12 15:09:41 +0100

    landing page slideshow

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import { Element } from "react-scroll";
 
 import Background1 from "./media/background1.jpg";
@@ -6,9 +7,19 @@ import Background3 from "./media/background3.jpg";
 import Background4 from "./media/background4.jpg";
 
 const Landing = () => {
+  const [selectedBackground, setSelectedBackground] = useState(3);
+
   const backgroundArray = [Background1, Background2, Background3, Background4];
 
-  return <Element id="test1" className="element landing"></Element>;
+  useEffect(() => {}, []);
+
+  return (
+    <Element
+      id="test1"
+      className="element landing"
+      style={{ backgroundImage: `url(${backgroundArray[selectedBackground]})` }}
+    ></Element>
+  );
 };
 
 export default Landing;
