0b54dfe0179da99056da09b4260c271f7c2c448b
dougiehawes <dougiehawes@hotmail.com>
2022-09-12 15:14:31 +0100

    landing page slideshow

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import { Element } from "react-scroll";
 
 import Background1 from "./media/background1.jpg";
@@ -7,17 +7,38 @@ import Background3 from "./media/background3.jpg";
 import Background4 from "./media/background4.jpg";
 
 const Landing = () => {
-  const [selectedBackground, setSelectedBackground] = useState(3);
+  const [index, setIndex] = useState(0);
 
   const backgroundArray = [Background1, Background2, Background3, Background4];
 
-  useEffect(() => {}, []);
+  const timeoutRef = useRef(null);
+
+  function resetTimeout() {
+    if (timeoutRef.current) {
+      clearTimeout(timeoutRef.current);
+    }
+  }
+
+  useEffect(() => {
+    resetTimeout();
+    timeoutRef.current = setTimeout(
+      () =>
+        setIndex((prevIndex) =>
+          prevIndex === backgroundArray.length - 1 ? 0 : prevIndex + 1
+        ),
+      6000
+    );
+
+    return () => {
+      resetTimeout();
+    };
+  }, [index]);
 
   return (
     <Element
       id="test1"
       className="element landing"
-      style={{ backgroundImage: `url(${backgroundArray[selectedBackground]})` }}
+      style={{ backgroundImage: `url(${backgroundArray[index]})` }}
     ></Element>
   );
 };
